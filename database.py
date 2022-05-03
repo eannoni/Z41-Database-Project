c1e14969a28a4b05845376e53ee6a69c025d219a
Scott Fitzpatrick <sfitzpatrick@chapman.edu>
2022-05-03 14:06:58 -0700

    Added database.py

---
+++ database.py
@@ -0,0 +1,146 @@
+import sys
+import mysql.connector
+
+class database:
+
+    def connectToMySQL(username, pw):#return the database object connecting to the correct schema
+        return mysql.connector.connect(host = "localhost", user = username, password = pw, auth_plugin = 'mysql_native_password', database = "Z41")
+
+    def ensureTablesExist(mydb, mycursor): 
+        #store the queries to create the tables
+        tableCreationQueries = [
+            '''
+            CREATE TABLE IF NOT EXISTS Customer(
+                CustomerID INT AUTO_INCREMENT PRIMARY KEY,
+                Name VARCHAR(50) NOT NULL,
+                Email VARCHAR(62) NOT NULL,
+                Address VARCHAR(95) NOT NULL
+            );
+            ''',
+            '''
+            CREATE TABLE IF NOT EXISTS Product(
+                ProductID INT AUTO_INCREMENT PRIMARY KEY,
+                Name VARCHAR(50) NOT NULL,
+                Description VARCHAR(280),
+                Price DECIMAL(8,2) NOT NULL
+            );
+            ''',
+            '''
+            CREATE TABLE IF NOT EXISTS Purchase(
+            PurchaseID INT AUTO_INCREMENT PRIMARY KEY,
+            CustomerID INT,
+            ProductID INT,
+            Date DATETIME NOT NULL,
+            Quantity INT NOT NULL,
+            FOREIGN KEY (ProductID) REFERENCES Product(ProductID),
+            FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID)
+            );
+            ''',
+            '''
+            CREATE TABLE IF NOT EXISTS Developer(
+                DeveloperID INT AUTO_INCREMENT PRIMARY KEY,
+                Name VARCHAR(50) NOT NULL ,
+                Email VARCHAR(62) NOT NULL ,
+                Address VARCHAR(95) NOT NULL ,
+                AvailableRolls INT NOT NULL
+            );
+            ''',
+            '''
+            CREATE TABLE IF NOT EXISTS FilmOrder(
+                OrderID INT AUTO_INCREMENT PRIMARY KEY,
+                CustomerID INT,
+                DeveloperID INT,
+                Status VARCHAR(20) NOT NULL ,
+                DatePlaced DATETIME NOT NULL ,
+                DateDelivered DATETIME,
+                Link VARCHAR(40),
+                Quantity INT NOT NULL ,
+                Price DECIMAL(8,2) ,
+                FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
+                FOREIGN KEY (DeveloperID) REFERENCES Developer(DeveloperID)
+            );
+            '''
+        ]
+        #execute the queries
+        for query in tableCreationQueries:
+            mycursor.execute(query)
+            mydb.commit()
+
+    def insertDataIfEmptyTables(mydb, mycursor):
+        #set the tables, and the values to insert into them into a dictionary
+        tables = {
+            "Customer": '''
+                        INSERT INTO Customer(Name, Email, Address) VALUES("Eli Annoni", "annoni@chapman.edu", "1 University Drive Orange, CA 92866"),
+                                                                        ("Ian Hakeman", "hakeman@chapman.edu", "1 University Drive Orange, CA 92866");
+                        ''',
+            "Developer": '''
+                        INSERT INTO Developer(Name, Email, Address, AvailableRolls) VALUES("Curren Taber", "ctaber@chapman.edu", "1 University Drive Orange, CA 92866", 5),
+                                                                                        ("Scott Fitzpatrick", "sfitzpatrick@chapman.edu", "915 E Katella Ave Anaheim, CA 92805", 28);
+                        ''', 
+            "Product": '''
+                        INSERT INTO Product(Name, Description, Price) VALUES("Zine41: Winter - Limited Print", NULL, 20.00), ("Liford HP65 35mm 36 Exposure Roll", "35mm film", 7.00);
+            ''',
+            "Purchase": '''
+                        INSERT INTO Purchase(CustomerID, ProductID, Date, Quantity) VALUES(1, 2, "2022-03-12 08:40:20", 6), (2, 1, "2022-04-29 14:05:07", 3);
+            ''', 
+            "FilmOrder": '''
+                        INSERT INTO FilmOrder(CustomerID, DeveloperID, Status, DatePlaced, DateDelivered, Link, Quantity, Price) VALUES(1, 2, "Complete", "2022-02-19 08:32:01", "2022-03-01 18:43:23", "www.chapman.edu", 16, 160.00), (2, 1, "In Progress", "2022-04-27 05:24:17", NULL, NULL, 5, 50.00);
+            '''
+        } 
+        #loop through the dictionary and see if tables are empty, if so, run the insert queries
+        for table, query in tables.items():
+            mycursor.execute("SELECT COUNT(*) FROM " + table + ";")
+            count = mycursor.fetchone()[0]
+            if count < 1:#if empty
+                mycursor.execute(query)
+                mydb.commit()
+
+    def endDB(mydb):
+        mydb.commit()
+        mydb.close()
+
+    def startDB(username, pw):
+        try:#connect to the correct database given username and password
+            mydb = database.connectToMySQL(username, pw)
+        except:
+            try:#if that failed, try to connect and make the schema
+                mydb = mysql.connector.connect(host = "localhost", user = username, password = pw, auth_plugin = 'mysql_native_password')
+                mycursor = mydb.cursor()
+                #create the DB
+                mycursor.execute("CREATE SCHEMA Z41;")
+                #then try connecting again to the schema we just made
+                mydb = database.connectToMySQL(username, pw)
+            except:#if that also failed, username and password is wrong
+                print("Incorrect password for username", username)
+                sys.exit()#quit since we couldn't connect
+
+        #print the connection
+        print(mydb)
+
+        #create cursor object to interact with mySQL
+        mycursor = mydb.cursor()
+
+        #make the tables if they don't exist
+        database.ensureTablesExist(mydb, mycursor)
+        print("Tables exist")
+
+        database.insertDataIfEmptyTables(mydb, mycursor)
+        print("Tables have starting data in them")
+
+        return [mydb, mycursor]
+
+#######################################################################################################
+########### THE REST OF THE CODE IS FOR TESTING AND MAKING IT STANDALONE, DELETE LATER ###############
+
+argv = sys.argv #store command line arguments
+if len(argv) < 3:#ensure there are at least 3 command line arguments 
+    print("Username and then password should be input as command line arguments for MySQL.")
+    sys.exit()#quit the executable 
+
+#this tests the return status of startDB
+list = database.startDB(argv[1], argv[2])
+'''
+print(list[0])
+print(list[1])
+'''
+database.endDB(list[0])
